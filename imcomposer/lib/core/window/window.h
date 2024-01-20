b83ab38aa606a35c9c1a8f9d99013adb6651d7b6
uventura <ualivent@gmail.com>
2024-01-20 12:06:07 -0300

    Adding main Window Class

---
+++ imcomposer/lib/core/window/window.h
@@ -0,0 +1,37 @@
+#ifndef IMCOMPOSER_WINDOW
+#define IMCOMPOSER_WINDOW
+
+#include <string>
+
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+namespace ImComposer
+{
+    class Window
+    {
+        private:
+            const int glfwMajorVersion_ = 3;
+            const int glfwMinorVersion_ = 0;
+            const int glfwMaximized_ = 1;
+
+            const std::string title_ = "ImComposer";
+            const int width_ = 640;
+            const int height_ = 480;
+            GLFWwindow* window_;
+            const float backgroundColor_[4] = {0.03f, 0.0f, 0.1f, 0.03f};
+
+        public:
+            Window();
+            ~Window();
+
+            void clear();
+            void bufferUpdate();
+            void renderUpdate();
+            inline int shouldClose(){return glfwWindowShouldClose(window_);};
+
+            inline GLFWwindow* getWindow(){return window_;};
+    };
+}
+
+#endif
