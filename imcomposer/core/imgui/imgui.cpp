d6dcfd98724fce0805e65b76e563dd7511df8277
uventura <ualivent@gmail.com>
2024-07-07 14:14:16 +0200

    Disabling multiviewport

--- imcomposer/core/imgui/imgui.cpp
+++ imcomposer/core/imgui/imgui.cpp
@@ -19,7 +19,7 @@ ImComposer::Core::Imgui::Imgui(GLFWwindow* window)
     io = &ImGui::GetIO(); (void)io;
     io->ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
     io->ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
-    io->ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+    // io->ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
     io->ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
 
     // Setup Dear ImGui style
